68d66e1947537620549f05c85ed6735532a0318c
hussnag <husnag768@gmail.com>
2025-07-08 01:27:39 +0500

    nn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiRootURL: 'http://103.74.54.207:8181/api/'  
+  // apiRootURL: 'http://103.74.54.207:8181/api/'  
+   apiRootURL: 'http://103.74.54.207:8080/api/'
 };
